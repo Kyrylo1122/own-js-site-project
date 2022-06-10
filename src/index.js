f0683054714ae0cf82527200e4fc0df3e689f00b
Kyrylo1122 <93613807+Kyrylo1122@users.noreply.github.com>
2022-06-10 13:57:36 +0200

    add axios

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-
 // import './js/go';
+import './js/fetchAPI';
--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
+
 const API_KEY = '9b627b5c13f01f3f3d8206e8b910ae85';
+async function fetchFilms() {
+  const res = await axios(
+    'https://api.themoviedb.org/3/movie/550?api_key=9b627b5c13f01f3f3d8206e8b910ae85'
+  );
+  return res;
+}
+
+fetchFilms().then(res => {
+  console.log(44);
+  console.log(res);
+});
